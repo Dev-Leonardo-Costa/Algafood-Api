719fbf80d069bdae7d1fbf0180c08d5ce1e24ddb
Leonardo Costa <devleonardo27@gmail.com>
2022-07-22 20:27:04 -0300

    endpoint de remover uma cozinha e um estado implementado

--- algafood/src/main/java/com/algafood/api/controller/CozinhaController.java
+++ algafood/src/main/java/com/algafood/api/controller/CozinhaController.java
@@ -2,11 +2,19 @@ package com.algafood.api.controller;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algafood.api.model.CozinhaXmlWrapper;
@@ -19,19 +27,59 @@ public class CozinhaController {
 
 	@Autowired
 	private CozinhaRepository cozinhaRepository;
-	
+
 	@GetMapping
-	public List<Cozinha> buscarTodas(){
-		return cozinhaRepository.buscarTodas();
+	public ResponseEntity<List<Cozinha>> buscarTodas() {
+		return ResponseEntity.ok().body(cozinhaRepository.buscarTodas());
 	}
-	
+
 	@GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
-	public CozinhaXmlWrapper buscarTodasXml(){
+	public CozinhaXmlWrapper buscarTodasXml() {
 		return new CozinhaXmlWrapper(cozinhaRepository.buscarTodas());
 	}
-	
+
 	@GetMapping("/{cozinhaId}")
-	public Cozinha buscarPorId(@PathVariable Long cozinhaId) {	
-		return cozinhaRepository.buscarPorId(cozinhaId);
+	public ResponseEntity<Cozinha> buscarPorId(@PathVariable Long cozinhaId) {
+		Cozinha cozinha = cozinhaRepository.buscarPorId(cozinhaId);
+
+		if (cozinha != null) {
+			return ResponseEntity.ok(cozinha);
+		}
+
+		return ResponseEntity.notFound().build();
+
 	}
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cozinha salvar(@RequestBody Cozinha cozinha) {
+		return cozinhaRepository.adicionar(cozinha);
+	}
+
+	@PutMapping("/{cozinhaId}")
+	public ResponseEntity<Cozinha> atualizar(@PathVariable Long cozinhaId, @RequestBody Cozinha cozinha) {
+
+		Cozinha cozinhaAtual = cozinhaRepository.buscarPorId(cozinhaId);
+
+		if (cozinhaAtual != null) {
+			// cozinhaPersistida.setNome(cozinha.getNome());
+			BeanUtils.copyProperties(cozinha, cozinhaAtual, "id");
+			cozinhaRepository.adicionar(cozinhaAtual);
+			return ResponseEntity.ok(cozinhaAtual);
+		}
+		return ResponseEntity.notFound().build();
+	}
+
+	@DeleteMapping("/{cozinhaId}")
+	public ResponseEntity<Cozinha> remover(@PathVariable Long cozinhaId) {
+		Cozinha cozinha = cozinhaRepository.buscarPorId(cozinhaId);
+
+		if (cozinha != null) {
+			cozinhaRepository.remover(cozinha);
+			return ResponseEntity.noContent().build();
+		}
+
+		return ResponseEntity.notFound().build();
+	}
+
 }
--- algafood/src/main/java/com/algafood/api/controller/EstadoController.java
+++ algafood/src/main/java/com/algafood/api/controller/EstadoController.java
@@ -8,10 +8,14 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algafood.api.model.EstadoXmlWrapper;
+import com.algafood.domain.model.Cozinha;
 import com.algafood.domain.model.Estado;
 import com.algafood.domain.repository.EstadoRepository;
 
@@ -37,11 +41,17 @@ public class EstadoController {
 		Estado estado = estadoRepository.buscaPorId(estadoId);
 
 		if (estado != null) {
-			return ResponseEntity.ok().body(estado);
+			return ResponseEntity.ok(estado);
 		}
 
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-
+//		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		return ResponseEntity.notFound().build();
 	}
-
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Estado salvar(@RequestBody Estado estado) {
+		return estadoRepository.adicionar(estado);
+	}
+	
 }

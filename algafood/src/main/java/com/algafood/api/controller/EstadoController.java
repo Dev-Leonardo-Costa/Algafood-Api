19f8727e9d49ac8fc4ae335673a0b01a0c263efa
Leonardo Costa <devleonardo27@gmail.com>
2022-07-27 15:32:35 -0300

    Endpoint de atualização implementado

--- algafood/src/main/java/com/algafood/api/controller/EstadoController.java
+++ algafood/src/main/java/com/algafood/api/controller/EstadoController.java
@@ -2,6 +2,7 @@ package com.algafood.api.controller;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -17,7 +19,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.algafood.domain.exception.EntidadeEmUsoException;
 import com.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algafood.domain.model.Estado;
-import com.algafood.domain.repository.EstadoRepository;
 import com.algafood.domain.service.CadastroEstadoService;
 
 @RestController
@@ -48,6 +49,20 @@ public class EstadoController {
 	@ResponseStatus(HttpStatus.CREATED)
 	public Estado salvar(@RequestBody Estado estado) {
 		return cadastroEstados.salvar(estado);
+	}
+	
+	@PutMapping("/{estadoId}")
+	public ResponseEntity<Estado> atualizar(@PathVariable Long estadoId, @RequestBody Estado estado){
+		Estado estadoAtual = cadastroEstados.buscarPorId(estadoId);
+		
+		if(estadoAtual != null) {
+			BeanUtils.copyProperties(estado, estadoAtual, "id");
+			
+			estadoAtual = cadastroEstados.salvar(estadoAtual);
+			return ResponseEntity.ok(estadoAtual);
+		}
+		
+		return ResponseEntity.notFound().build();
 	}
 
 	@DeleteMapping("/{estadoId}")

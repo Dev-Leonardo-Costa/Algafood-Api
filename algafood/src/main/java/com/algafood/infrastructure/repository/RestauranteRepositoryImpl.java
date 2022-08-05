91062e647f97153ad891b8ff4c3e5375f019fcad
Leonardo Costa <devleonardo27@gmail.com>
2022-08-05 10:45:10 -0300

    Tornando a consulta com criteria API com filtro dinamico

--- algafood/src/main/java/com/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ algafood/src/main/java/com/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -1,6 +1,7 @@
 package com.algafood.infrastructure.repository;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -8,8 +9,11 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import com.algafood.domain.model.Restaurante;
 import com.algafood.domain.repository.RestauranteRepositoryQueries;
@@ -23,17 +27,37 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 	@Override
 	public List<Restaurante> find(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
 
-		CriteriaBuilder builder = manager.getCriteriaBuilder();
+//		CriteriaBuilder builder = manager.getCriteriaBuilder();
+		var builder = manager.getCriteriaBuilder();
 		
-		CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class);
-		criteria.from(Restaurante.class);
+//		CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class);
+//		Root<Restaurante> root = criteria.from(Restaurante.class);
+		var criteria = builder.createQuery(Restaurante.class);
+		var root = criteria.from(Restaurante.class);
 		
-		TypedQuery<Restaurante> query = manager.createQuery(criteria);
+		var predicates = new ArrayList<Predicate>();
+
+		if (StringUtils.hasText(nome)) {
+			predicates.add(builder.like(root.get("nome"), "%" + nome + "%"));
+		}
+		
+		if (taxaFreteInicial != null) {
+			predicates.add(builder.greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteInicial));
+		}
+
+		if (taxaFreteFinal != null) {
+			predicates.add(builder.greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal));
+		}
+
+		criteria.where(predicates.toArray(new Predicate[0]));
+
+//		TypedQuery<Restaurante> query = manager.createQuery(criteria);
+		var query = manager.createQuery(criteria);
 		return query.getResultList();
 //		return manager.createQuery(criteria).getResultList();
+
 	}
 }
-
 
 // consulta do metodo find
 //var jpql = new StringBuilder();

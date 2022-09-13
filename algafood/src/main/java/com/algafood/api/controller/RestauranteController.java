d673c143f026f24e0e406481d3f07204816712d6
Leonardo Costa <devleonardo27@gmail.com>
2022-09-13 14:20:32 -0300

    trabalhando com JPA

--- algafood/src/main/java/com/algafood/api/controller/RestauranteController.java
+++ algafood/src/main/java/com/algafood/api/controller/RestauranteController.java
@@ -34,7 +34,16 @@ public class RestauranteController {
 
 	@GetMapping
 	public List<Restaurante> buscarTodos() {
-		return cadastroRestaurante.buscarTodos();
+		List<Restaurante> restaurantes = cadastroRestaurante.buscarTodos();
+		
+		System.out.println(restaurantes.get(0).getNome());
+		restaurantes.get(0).getFormasPagamento().forEach(System.out::println);
+		
+		System.out.println(restaurantes.get(1).getNome());
+		restaurantes.get(1).getFormasPagamento().forEach(System.out::println);
+
+		
+		return restaurantes;
 	}
 
 	@GetMapping("/{restauranteId}")

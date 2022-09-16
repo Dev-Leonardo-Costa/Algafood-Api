beb5a2f694cf0d062574f1924320639d82676f6f
Leonardo Costa <devleonardo27@gmail.com>
2022-09-16 17:53:37 -0300

    entidades pedido e itens_pedido implementada com suas respectivas migration

---
+++ algafood/src/main/java/com/algafood/domain/model/Pedido.java
@@ -0,0 +1,44 @@
+package com.algafood.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Pedido {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal subTotal;
+    private BigDecimal taxaFrete;
+    private BigDecimal valorTotal;
+
+    @Embedded
+    private Endereco enderecoEntrega;
+
+    private StatusPedido status;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private FormaPagamento formaPagamento;
+
+    @ManyToOne
+    @JoinColumn(nullable = true)
+    private Restaurante restaurante;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_cliente_id")
+    private Usuario cliente;
+
+    @OneToMany(mappedBy = "pedido")
+    private List<ItemPedido> itens = new ArrayList<>();
+}

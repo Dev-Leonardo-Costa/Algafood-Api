a04fb433ccf22f15cb44e9f916412d1348ef4742
Leonardo Costa <devleonardo27@gmail.com>
2022-10-26 06:45:51 -0300

    9.4. Estendendo o Problem Details para adicionar as propriedades com constraints violadas

--- algafood/src/main/java/com/algafood/exceptionhandler/Problem.java
+++ algafood/src/main/java/com/algafood/exceptionhandler/Problem.java
@@ -4,6 +4,7 @@ import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @Builder
@@ -15,5 +16,13 @@ public class Problem {
     private String title;
     private String detail;
     private String userMessage;
+    private List<Field> fields;
+
+    @Getter
+    @Builder
+    public static class Field {
+       private String name;
+        private String userMessage;
+    }
 
 }

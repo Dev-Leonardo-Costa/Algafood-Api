ab8b21be1e53f8be28c55d6f979c686c1fe65e17
Leonardo Costa <devleonardo27@gmail.com>
2022-10-31 16:30:47 -0300

    11.3 Criando classes de mixin para usar as anotações do Jackson

---
+++ algafood/src/main/java/com/algafood/core/jackson/JacksonMixinModule.java
@@ -0,0 +1,14 @@
+package com.algafood.core.jackson;
+
+import com.algafood.domain.model.Restaurante;
+import com.algafood.domain.model.mixin.RestauranteMixin;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JacksonMixinModule extends SimpleModule {
+
+    public JacksonMixinModule() {
+        setMixInAnnotation(Restaurante.class, RestauranteMixin.class);
+    }
+}
